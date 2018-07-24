4a52716c5fe626bd0bda48241fed267d5925ace4
MonicaDumencu <monica_dumencu@yahoo.com>
2018-07-24 10:40:19 +0200

    Echo back messages

--- app.js
+++ app.js
@@ -1,64 +1,29 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+var restify = require('restify');
+var builder = require('botbuilder');
 
-const { BotFrameworkAdapter, MessageFactory, CardFactory } = require('botbuilder');
-const restify = require('restify');
+var Connect = 'Connect';
+var Upload = 'Show uploaded attachment';
+var External = 'Show Internet attachment';
+var Options = [Connect, Upload, External];
 
-// Create server
-let server = restify.createServer();
+var inMemoryStorage = new builder.MemoryBotStorage();
+
+// Setup Restify Server
+var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-    console.log(`${server.name} listening to ${server.url}`);
+   console.log('%s listening to %s', server.name, server.url); 
 });
 
-// Create adapter
-const adapter = new BotFrameworkAdapter({
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
     appId: process.env.MicrosoftAppId,
     appPassword: process.env.MicrosoftAppPassword
 });
 
-// Responds to the incoming message by either sending a hero card, an image, 
-// or echoing the user's message.
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
 
-// Listen for incoming requests 
-server.post('/api/messages', (req, res) => {
-    // Route received request to adapter for processing
-    adapter.processActivity(req, res, (context) => {
-        if (context.activity.type === 'message') {
-            const text = (context.activity.text || '').trim().toLowerCase()
-
-            switch(text){
-                case 'hi':
-                case "hello":
-                case "help":
-                    // Send the user an instruction message.
-                    return context.sendActivity("Welcome to the Bot to showcase the DirectLine API. " +
-                    "Send \"Show me a hero card\" or \"Send me a BotFramework image\" to see how the " +
-                    "DirectLine client supports custom channel data. Any other message will be echoed.");
-                    break;
-
-                case 'show me a hero card':
-                    // Create the hero card.
-                    const message = MessageFactory.attachment(
-                        CardFactory.heroCard(   
-                        'Sample Hero Card', //cards title
-                        'Displayed in the DirectLine client' //cards text
-                        )
-                    );
-                    return context.sendActivity(message);
-                    break;
-                    
-                case 'send me a botframework image':
-                    // Create the image attachment.
-                    const imageOrVideoMessage = MessageFactory.contentUrl('https://docs.microsoft.com/en-us/azure/bot-service/media/how-it-works/architecture-resize.png', 'image/png')
-                    return context.sendActivity(imageOrVideoMessage);
-                    break;
-                
-                default:
-                    // No command was encountered. Echo the user's message.
-                    return context.sendActivity(`You said ${context.activity.text}`);
-                    break;
-                    
-            }
-        }
-    });
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said: %s", session.message.text);
 });
