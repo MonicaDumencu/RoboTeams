cc4b8f0271e9827e9e810ff21a3e0b3e42bc21e2
manajdov <manajdov@microsoft.com>
2018-07-25 14:44:31 +0200

    -alternative to cards

--- app.js
+++ app.js
@@ -11,154 +11,153 @@ var inMemoryStorage = new builder.MemoryBotStorage();
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-  console.log('%s listening to %s', server.name, server.url);
+   console.log('%s listening to %s', server.name, server.url); 
 });
 
 // Create chat connector for communicating with the Bot Framework Service
 var connector = new builder.ChatConnector({
-  appId: process.env.MicrosoftAppId,
-  appPassword: process.env.MicrosoftAppPassword
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
 });
 
 // Listen for messages from users 
 server.post('/api/messages', connector.listen());
-const commands = ['STRAIGHT', 'LEFT', 'RIGHT', 'BACK', 'STOP', 'CONNECT', 'DISCONNECT'];
 
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 var bot = new builder.UniversalBot(connector, function (session) {
-  if(commands.indexOf(session.message.text) > -1 ) {
-    //some valid command chosen
-    var msg = new builder.Message(session)
-      .addAttachment({
-        contentType: "application/vnd.microsoft.card.adaptive",
-        content: {
-          type: "AdaptiveCard",
-          speak: `<s>Your  meeting about \"You chose the command: ${session.message.text}\"<break strength='weak'/> Change the command to some of the following:</s>`,
-          body: [
-            {
-              "type": "TextBlock",
-              "text": "Welcome to the RoboTeams boot.",
-              "size": "large",
-              "weight": "bolder"
-            },
-            {
-              "type": "TextBlock",
-              "text": "Please type the name of your bluetooth device"
-            },
-            {
-              "type": "Input.Text",
-              "id": "name",
-              "style":"compact",
-            }
-          ],
-          "actions": [
-            {
-              "type": "Action.OpenUrl",
-              "method": "POST",
-              "url": "http://foo.com",
-              "title": "STRAIGHT"
-            },
-            {
-              "type": "Action.OpenUrl",
-              "method": "POST",
-              "url": "http://foo.com",
-              "title": "LEFT"
-            },
-            {
-              "type": "Action.OpenUrl",
-              "method": "POST",
-              "url": "http://foo.com",
-              "title": "RIGHT"
-            },
-            {
-              "type": "Action.OpenUrl",
-              "method": "POST",
-              "url": "http://foo.com",
-              "title": "BACK"
-            },
-            {
-              "type": "Action.OpenUrl",
-              "method": "POST",
-              "url": "http://foo.com",
-              "title": "STOP"
-            },
-            {
-              "type": "Action.OpenUrl",
-              "method": "POST",
-              "url": "http://foo.com",
-              "title": "DISCONNECT"
-            },
-            {
-              "type": "Action.OpenUrl",
-              "method": "POST",
-              "url": "http://foo.com",
-              "title": "CONNECT"
-            }
-          ]
-        }
-      });
-  } else {
-    var msg = new builder.Message(session)
-      .addAttachment({
-        contentType: "application/vnd.microsoft.card.adaptive",
-        content: {
-          type: "AdaptiveCard",
-          speak: "<s>Your  meeting about \"Welcome to the RoboTeams boot.\"<break strength='weak'/> Enter the name of your bluetooth device: </s>",
-          body: [
-            {
-              "type": "TextBlock",
-              "text": "Welcome to the RoboTeams boot.",
-              "size": "large",
-              "weight": "bolder"
-            },
-            {
-              "type": "TextBlock",
-              "text": "Please type the name of your bluetooth device"
-            },
-            {
-              "type": "Input.Text",
-              "id": "name",
-              "style":"compact",
-            }
-          ],
-          "actions": [
-            {
-              "type": "Action.OpenUrl",
-              "method": "POST",
-              "url": "http://foo.com",
-              "title": "Send"
-            },
-          ]
-        }
-      });
-  }
+    if (session.message && session.message.value) {
+        processSubmitAction(session, session.message.value);
+        return;
+	}
 
-  session.send(msg);
+	var msg = new builder.Message(session)
+    .text("What command would you like to send?")
+    .suggestedActions(
+        builder.SuggestedActions.create(
+                session, [
+                    builder.CardAction.imBack(session, "START", "Connect"),
+                    builder.CardAction.imBack(session, "FORWARDS", "Move Forwards"),
+                    builder.CardAction.imBack(session, "STOP", "Stop")
+                ]
+            ));
+	session.send(msg);
+
+}).set('storage', inMemoryStorage); // Register in memory storage
+
+bot.dialog('connect-car', require('./connect-car'));
+
+// log any bot errors into the console
+bot.on('error', function (e) {
+    console.log('And error ocurred', e);
 });
-// Order dinner.
-bot.dialog('orderDinner', [
-  //...waterfall steps...
-])
-// Once triggered, will start the 'showDinnerCart' dialog.
-// Then, the waterfall will resumed from the step that was interrupted.
-  .beginDialogAction('showCartAction', 'showDinnerCart', {
-    matches: /^show cart$/i,
-    dialogArgs: {
-      showTotal: true
+
+function processSubmitAction(session, value) {
+    var defaultErrorMessage = 'Not a command';
+    switch (value.type) {
+        case 'connect':
+            session.send('Roboteams: connect');
+            break;
+
+        default:
+            // A form data was received, invalid or incomplete since the previous validation did not pass
+            session.send(defaultErrorMessage);
     }
-  });
+}
+	
 
-// Show dinner items in cart with the option to show total or not.
-bot.dialog('showDinnerCart', function (session, args) {
-  for (var i = 1; i < session.conversationData.orders.length; i++) {
-    session.send(`You ordered: ${session.conversationData.orders[i].Description} for a total of $${session.conversationData.orders[i].Price}.`);
-  }
+/*var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        session.send('Welcome, here you can see attachment alternatives:');
+        builder.Prompts.choice(session, 'What sample option would you like to see?', Options, {
+            maxRetries: 3
+        });
+    },
+    function (session, results) {
+        var option = results.response ? results.response.entity : Inline;
+        switch (option) {
+            case Connect:
+                return connect();
+            case Upload:
+                return;
+            case External:
+                return;
+        }
+ }]).set('storage', inMemoryStorage); // Register in memory storage
+ 
+ function connect() {
+	"use strict";
+	var serverName = 'HC-06';	
 
-  if (args && args.showTotal) {
-    // End this dialog with total.
-    session.endDialog(`Your total is: $${session.conversationData.orders[0].Price}`);
-  }
-  else {
-    session.endDialog(); // Ends without a message.
-  }
-});
+	var util = require('util');
+	var DeviceINQ = require("./node_modules/bluetooth-serial-port/lib/device-inquiry.js").DeviceINQ;
+	var BluetoothSerialPort = require("./node_modules/bluetooth-serial-port/lib/bluetooth-serial-port.js").BluetoothSerialPort;
+	var serial = new BluetoothSerialPort();
+	const MAX_MSGS_SENT = 10;
+	var keepScanning = false;
+
+	serial.on('found', function (address, name) {
+		console.log('Found: ' + address + ' with name ' + name);
+
+		serial.findSerialPortChannel(address, function(channel) {
+			console.log('Found RFCOMM channel for serial port on ' + name + ': ' + channel);
+
+			if (name !== serverName) return;
+
+			console.log('Attempting to connect...');
+
+			serial.connect(address, channel, function() {
+		keepScanning  = false;
+			let packetsSent = 0;
+				console.log('Connected. Sending data...');
+				let buf = new Buffer('f');
+				console.log('Size of buf = ' + buf.length);
+
+		serial.on('failure', function(err){
+			console.log('Something wrong happened!!: err = ' + err);
+		});
+
+				serial.on('data', function(buffer) {
+					console.log('Received: Size of data buf = ' + buffer.length);
+					console.log(buffer.toString('utf-8'));
+			serial.write(buf, function(err,count){
+						if(err){
+							console.log('Error received: ' + err);
+				return;
+					}
+					
+			console.log('Sent: Bytes written: ' + count);
+			packetsSent++;
+			console.log('Sent: count = ' + packetsSent);
+			if(packetsSent == MAX_MSGS_SENT){
+				console.log('' + MAX_MSGS_SENT + ' sent!. Closing connection');
+				serial.close();
+				process.exit(0);
+			}
+			});
+				});
+
+				serial.write(buf, function(err, count) {
+					if (err) {
+						console.log('Error received: ' + err);
+					} else {
+						console.log('Bytes writen is: ' + count);
+					}
+				});
+			});
+		});
+	});
+
+	serial.on('close', function() {
+		console.log('connection has been closed (remotely?)');
+	});
+
+	serial.on('finished', function() {
+		console.log('Scan finished.');
+	if(keepScanning == true){
+		console.log('Rescanning..');
+		serial.inquire();
+	}
+	});
+
+	serial.inquire();
+}*/
